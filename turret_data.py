45bbc460e373c93093545691c8a77864d095f503
EthanK10 <164193602+EthanK10@users.noreply.github.com>
2024-06-05 10:57:39 -0400

    Add files via upload
    upload copy of project

---
+++ turret_data.py
@@ -0,0 +1,22 @@
+TURRET_DATA = [
+  {
+    #1
+    "range": 110,
+    "cooldown": 1200,
+  },
+  {
+    #2
+    "range": 125,
+    "cooldown": 1000,
+  },
+  {
+    #3
+    "range": 150,
+    "cooldown": 900,
+  },
+  {
+    #4
+    "range": 165,
+    "cooldown": 750,
+  }
+]
